0fb7aba7981279606c79130e12dee7f195dea413
0xG0nz0 <null>
2025-02-23 23:22:29 +0000

    Fix missing pre-install.sh; downgrade Pants; export venvs

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -3,6 +3,9 @@
 # install pants
 curl --proto '=https' --tlsv1.2 -sSfL https://pantsbuild.github.io/setup/get-pants.sh | bash
 
+# export the virtualenvs for use in VSCode
+pants export
+
 # install pre-commit hooks
 export PATH="/usr/local/py-utils/bin/pre-commit:$PATH"
 pre-commit install --install-hooks
--- pants-plugins/markdown/markdownlint.py
+++ pants-plugins/markdown/markdownlint.py
@@ -1,2 +1,24 @@
+from pants.backend.python.subsystems.python_tool_base import PythonToolBase
+from pants.backend.python.target_types import ConsoleScript
+from pants.option.option_types import StrOption
+
+
+class PyMarkdown(PythonToolBase):
+    options_scope = "black"
+    help = "The PyMarkdown linter (https://pymarkdown.readthedocs.io/)."
+
+    default_main = ConsoleScript("pymarkdown")
+
+    register_interpreter_constraints = True
+    default_interpreter_constraints = ["CPython>=3.12"]
+
+    default_lockfile_resource = ("markdown", "pymarkdown.lock")
+    config = StrOption(
+        default=None,
+        advanced=True,
+        help="Path to pymarkdown config file",
+    )
+
+
 def rules():
     return []
