90537504e6627aaaac45d7c97d7c2291d687285d
0xG0nz0 <null>
2025-03-01 17:01:28 +0000

    Remove all references to pants

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# install pants
-curl --proto '=https' --tlsv1.2 -sSfL https://pantsbuild.github.io/setup/get-pants.sh | bash
-
-# export the virtualenvs for use in VSCode
-pants export
-
 # install pre-commit hooks
 export PATH="/usr/local/py-utils/bin/pre-commit:$PATH"
 pre-commit install --install-hooks
