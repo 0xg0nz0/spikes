68aa707216fe79dbd41a0afa1a62821f884d74c8
0xG0nz0 <null>
2025-03-02 14:33:08 +0000

    Add basic tooling for Bazel

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# install Bazel CLI tools
+go install github.com/bazelbuild/buildtools/buildifier@latest
+go install github.com/bazelbuild/buildtools/buildozer@latest
+
 # install pre-commit hooks
 export PATH="/usr/local/py-utils/bin/pre-commit:$PATH"
 pre-commit install --install-hooks
